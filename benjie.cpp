d56825a822f6f2266b44075aa507ed3b1a1a98ae
benjieglenogo <glenogo16@gmail.com>
2024-08-24 17:28:37 +0800

    the failed and successfully directory

--- benjie.cpp
+++ benjie.cpp
@@ -8,12 +8,20 @@
 
     void DFiles(){
         system("dir");  // windows command ni!!
-        system("pause");  // mag pause ang screen kadyot!!!
+        system("pause");  // mag pause ang screen kadali!!!
     }
 
 void CRDirectory(){
     std::cout << "\nEnter the directory name: ";
     std::string dirName;
     std::cin >> dirName;
+    
+if (_mkdir(dirName.c_str()) == 0) {
+        std::cout << "\nDirectory '" << dirName << "' Successfully Created\n";
+    } else {
+        std::cout << "\nFailed to Create Directory '" << dirName << "'\n";
+    }
 
+    system("pause");
+}
    
