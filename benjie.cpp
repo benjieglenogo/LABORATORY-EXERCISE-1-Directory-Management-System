a860bb2c3fc63a71b3fd23260c35d1026eaa0436
benjieglenogo <glenogo16@gmail.com>
2024-08-26 13:24:03 +0800

    list of file

--- benjie.cpp
+++ benjie.cpp
@@ -35,4 +35,38 @@ int main() {
         }
     }
 }
-    
+    void display_main_menu() {
+    std::cout << "\nMAIN MENU\n";
+    std::cout << "1. Display List of Files\n";
+    std::cout << "2. Create New Directory\n";
+    std::cout << "3. Change the Working Directory\n";
+    std::cout << "4. Exit\n";
+    std::cout << "Enter the Number: ";
+}
+
+void list_files_menu() {
+    int choice;
+    std::cout << "\nLIST FILE DETAIL\n";
+    std::cout << "1. List All Files\n";
+    std::cout << "2. List Files by Extension\n";
+    std::cout << "3. List Files by Name\n";
+    std::cout << "Enter the Number: ";
+    std::cin >> choice;
+
+    switch (choice) {
+        case 1:
+            list_all_files();
+            break;
+        case 2:
+            std::cout << "Listing files by extension is not yet implemented.\n";
+            // Add functionality to list files by extension
+            break;
+        case 3:
+            std::cout << "Listing files by name is not yet implemented.\n";
+            // Add functionality to list files by name
+            break;
+        default:
+            std::cout << "Invalid choice. Please try again.\n";
+            break;
+    }
+}
