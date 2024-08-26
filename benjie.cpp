328733b04a20af33594700c5d6ca28a81a39c602
benjieglenogo <glenogo16@gmail.com>
2024-08-26 13:22:34 +0800

    choices

--- benjie.cpp
+++ benjie.cpp
@@ -1,17 +1,38 @@
-#include <string>
 #include <iostream>
-#include <cstdio> 
-#include <cstdlib> 
-#include <direct.h>
-#include <windows.h>
+#include <filesystem>
 
-void listFiles (const string&path) {
-  cout << "Listing files in Directory" << path << endl;
-  WIN32_FIND_DATA findFileData;
-  HANDLE hFind=FindFirstFile((path+"/*").c_str(),findFindData);
-  if(hFind==INVALID_HANDLE_VALUE){
-       cout <<"Error could not open directory"<< endl;
-       return;
-     }
-     
+namespace fs = std::filesystem;
+
+void display_main_menu();
+void list_files_menu();
+void list_all_files();
+void create_directory();
+void change_directory();
+
+int main() {
+    int choice;
+
+    while (true) {
+        display_main_menu();
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                list_files_menu();
+                break;
+            case 2:
+                create_directory();
+                break;
+            case 3:
+                change_directory();
+                break;
+            case 4:
+                std::cout << "Exiting the program...\n";
+                return 0;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+                break;
+        }
+    }
+}
     
